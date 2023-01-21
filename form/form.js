d458cc9a6b5fe21fb2747c6020a61616c035c6a3
Thoma Devdariani <devdarianitoma@gmail.com>
2023-01-21 12:27:09 +0400

    lmdm

--- form/form.js
+++ form/form.js
@@ -3,6 +3,12 @@ const email = document.getElementById("email");
 const country = document.getElementById("country");
 const city = document.getElementById("city");
 const zipcode = document.getElementById("zipcode");
+const wrapper = document.querySelector(".wrapper");
+const headerText = document.querySelector(".signUp-text");
+const headerDescription = document.querySelector(".signUp-description");
+const topOfForm = document.querySelector(".topOfForm");
+const input = document.querySelector("input");
+
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -58,3 +64,39 @@ const validateInputs = () => {
     setSuccess(zipcode);
   }
 }
+
+const check = document.getElementById("checkbox");
+
+check.addEventListener("change", function(){
+  if(this.checked){
+    wrapper.style.backgroundColor = "white";
+    headerText.style.color = "black";
+    headerDescription.style.color = "#748AA1";
+    form.style.backgroundColor = "white"
+    form.style.boxShadow = "box-shadow: 0px 1px 4px #E5E9F2;"
+    topOfForm.style.backgroundColor = "white";
+    email.style.backgroundColor = "white";
+    zipcode.style.backgroundColor = "white";
+    city.style.backgroundColor = "white";
+    select.style.backgroundColor = "white";
+    email.style.color = "black";
+    zipcode.style.color = "black";
+    city.style.color = "black";
+    select.style.color = "black";
+  }else{
+    wrapper.style.backgroundColor = "#1f2327";
+    headerText.style.color = "white";
+    headerDescription.style.color = "#748AA1";
+    form.style.backgroundColor = "#292E33";
+    form.style.boxShadow = "box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.2)"
+    topOfForm.style.backgroundColor = "#292E33";
+    email.style.backgroundColor = "#292E33";
+    zipcode.style.backgroundColor = "#292E33";
+    city.style.backgroundColor = "#292E33";
+    select.style.backgroundColor = "#292E33";
+    email.style.color = "white";
+    zipcode.style.color = "white";
+    city.style.color = "white";
+    select.style.color = "white";
+  }
+})
